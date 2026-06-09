d5ee93f563abc7d8ac62fba04c0035b0b9bdccee
Lysander <haoshiyao33@gmail.com>
2026-06-09 19:57:13 +0800

    搭建 note 应用骨架，配置中文语言环境
    创建 requirements.txt、配置中文语言和上海时区、创建 note Django 应用

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 自定义应用
+    "note",
 ]
 
 MIDDLEWARE = [
@@ -102,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Shanghai"
 
 USE_I18N = True
 
